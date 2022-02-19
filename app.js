a50baef29885feb39676cf5b263d4ac9e738beb1
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-02-19 11:45:13 +0700

    feat: setup ejs

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 3000;
-
 app.use(express.json());
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 
     var today = new Date();
     var currentDay = today.getDay();
+    var day = "";
 
-    if (currentDay === 6 || currentDay === 0 ){
-        res.sendFile(__dirname + '/index.html');
-        //res.send();
-    }else{
-        res.send("<h1>Weekdays</h1>");
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+         case 5:
+            day = "Friday";
+            break;  
+        case 6:
+            day = "Saturday";
+            break; 
+        default:
+            console.log("Error: current day is equal to L " + currentDay);
     }
+    res.render("list", {
+        kindOfDay: day
+    })
     //res.send('Hola');
 });
 
