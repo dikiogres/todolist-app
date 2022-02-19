b6932029664dd3ccf754aa8b92ef7d920260a839
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-02-19 13:36:24 +0700

    change: var to let variable

--- app.js
+++ app.js
@@ -10,25 +10,25 @@ app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
-var items = [
+let items = [
     "Study",
     "Work",
     "Study"
 ];
 
-var workItems = [];
+let workItems = [];
 
 app.get('/', (req, res) => {
 
-    var today = new Date();
+    let today = new Date();
 
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
 
     res.render("list", {
         listTitle: day, 
@@ -39,7 +39,7 @@ app.get('/', (req, res) => {
 
 app.post("/", (req, res)=> {
 
-    var item= req.body.newItem;
+    let item= req.body.newItem;
 
     if (req.body.list === "Work"){
         workItems.push(item);
@@ -60,7 +60,7 @@ app.get("/work", (req, res)=> {
 })
 
 // app.post("/work", (req, res)=> {
-//     var item = req.body.newItem;
+//     let item = req.body.newItem;
 //     workItems.push(item);
 //     res.redirect("/work");
 // });
